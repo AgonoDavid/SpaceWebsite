bdf6a2cee0f666b49f500bc2dbeeba7adfc38feb
AgonoDavid <davidagono239@gmail.com>
2023-10-16 07:11:54 +0100

    new

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -46,7 +46,7 @@ export default function Header() {
                 <line x1="6" y1="6" x2="18" y2="18" />
               </svg>
             </div>
-            <ul className="MENU-LINK-MOBILE-OPEN  flex flex-col items-center justify-between min-h-[250px]">
+            <ul className="MENU-LINK-MOBILE-OPEN  text-white flex flex-col items-center justify-between min-h-[250px]">
               <li className="border-b border-gray-400 my-8 uppercase">
                 <Link to="/">Home</Link>
               </li>
